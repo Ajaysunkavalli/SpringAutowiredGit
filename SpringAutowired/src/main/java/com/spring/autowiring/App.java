b611339d1380e087f4231c6ce777e7a98c28250e
Ajaysunkavalli <Ajay Sunkavalli@LAPTOP-BVJNCKA6>
2017-02-24 14:48:08 -0500

    Commiting 1

---
+++ SpringAutowired/src/main/java/com/spring/autowiring/App.java
@@ -0,0 +1,16 @@
+package com.spring.autowiring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.spring.autowired.Customer;
+
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext context=new ClassPathXmlApplicationContext("Spring.xml");
+		Customer cust=(Customer) context.getBean("customer");
+		System.out.println(cust);
+		
+	}
+
+}
